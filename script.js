4b4db130242b6e354a41fb58a37c140181d1e893
YBoah <yawboahene.yb@gmail.com>
2023-09-27 23:25:39 +0000

    More functionality

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ const body = document.querySelector("body"),
    secondHand = document.querySelector(".second"),
    modeSwitch = document.querySelector(".mode-switch");
 
+   // check if the mode is already set to "Dark Mode" in localStorage
+   if(localStorage.getItem("mode") === "Dark Mode"){
+    // add "dark" class to body and set modeSwitch text to "Light Mode"
+       body.classList.add("dark");
+       modeSwitch.textContent = "Light Mode";
+   }
+
+   // add a click event listener to modeSwitch
+   modeSwitch.addEventListener("click", () => {
+     // toggle the "dark" class on the body element
+     body.classList.toggle("dark");
+     // check if the "dark" class is currently present on the body element
+     const isDarkMode = body.classList.contains("dark");
+     // set modeSwitch text based on "dark" class presence
+     modeSwitch.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
+     // set localStorage "mode" item based on "dark" class presence
+     localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode");
+   })
+
    const updateTime = () => {
     // Get current time and calculate degrees for clock hands
     let date = new Date(),
